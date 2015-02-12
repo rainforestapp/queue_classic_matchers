2471981f29752739dd23e50f0ab82a9c506fa010
Simon Mathieu <simon.math@gmail.com>
2015-02-11 17:33:35 -0800

    Added some unit tests

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in queue_classic_matchers.gemspec
 gemspec
+
+gem 'rspec'
+gem "queue_classic_plus", github: 'rainforestapp/queue_classic_plus', branch: 'qc-3-1'
+
---
+++ spec/queue_classic_matchers_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe QueueClassicMatchers do
+  class TestJob < QueueClassicPlus::Base
+    @queue = :low
+    def self.perform
+    end
+  end
+
+  describe "have_queued" do
+    it "works with no arguments" do
+      expect(TestJob).to_not have_queued
+      TestJob.do
+      expect(TestJob).to have_queued
+    end
+
+    it "works wiht argument" do
+      expect(TestJob).to_not have_queued(1)
+      TestJob.do 1
+      expect(TestJob).to have_queued(1)
+      expect(TestJob).to_not have_queued(2)
+    end
+  end
+
+  describe "have_queue_size_of" do
+    it "works" do
+      expect(TestJob).to have_queue_size_of(0)
+      expect(TestJob).to_not have_queue_size_of(1)
+
+      TestJob.do
+
+      expect(TestJob).to have_queue_size_of(1)
+      expect(TestJob).to_not have_queue_size_of(0)
+    end
+  end
+end
